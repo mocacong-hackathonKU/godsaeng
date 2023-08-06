adb08e9955b56a6e83463fcb2c9772c4a8f8655c
임지수 <imjisu@imjisuui-MacBookPro.local>
2023-08-07 00:35:07 +0900

    test: 마이페이지 조회 관련 테스트 코드 추가

--- server/src/main/java/godsaeng/server/repository/MemberProfileImageRepository.java
+++ server/src/main/java/godsaeng/server/repository/MemberProfileImageRepository.java
@@ -1,10 +1,8 @@
-package mocacong.server.repository;
+package godsaeng.server.repository;
 
-import java.util.List;
-import mocacong.server.domain.MemberProfileImage;
+import godsaeng.server.domain.MemberProfileImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberProfileImageRepository extends JpaRepository<MemberProfileImage, Long> {
 
-    List<MemberProfileImage> findAllByIsUsedFalse();
 }
