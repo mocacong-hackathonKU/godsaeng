c0785486f1546ca757b95c12ce17ff4742a446cc
sujileelea <sujileelea@gmail.com>
2023-08-07 14:06:12 +0900

    rename: LoginView file created

---
+++ iOS/godsaeng/godsaeng/LoginPage.swift
@@ -0,0 +1,20 @@
+//
+//  LoginPage.swift
+//  godsaeng
+//
+//  Created by Suji Lee on 2023/08/07.
+//
+
+import SwiftUI
+
+struct LoginPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginPage_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginPage()
+    }
+}
