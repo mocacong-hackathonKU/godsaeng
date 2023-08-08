b75ce267ed4b380cf51aeee7a8e234cb7e02242c
ddiyooong <jsshin000107@naver.com>
2023-08-08 21:25:38 +0900

    feat: BaseActivity 및 Fragment 추가

---
+++ android/GodSeang/app/src/main/java/com/mocacong/godsaeng/base/BaseActivity.kt
@@ -0,0 +1,30 @@
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<T : ViewDataBinding>
+    (@LayoutRes private val layoutId: Int) : AppCompatActivity() {
+
+    protected lateinit var binding: T
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // 필요에 따라 OnCreate, OnStart에
+        // Log를 붙여 생명주기를 관찰할 수도 있음
+        //Log.d(localClassName,"OnCreate")
+
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutId)
+        initView()
+        initViewModel()
+        initListener()
+    }
+
+    protected open fun initView() {}
+    protected open fun initViewModel() {}
+
+    protected open fun initListener() {}
+
+}
