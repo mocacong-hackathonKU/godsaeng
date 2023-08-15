e512337cd23fbc4ea868b0494a707fa9f711b307
ddiyooong <jsshin000107@naver.com>
2023-08-16 02:38:20 +0900

    feat: DetailActivity 기본 뷰 구현

---
+++ android/GodSeang/app/src/main/java/com/mocacong/godsaeng/view/detail/DetailActivity.kt
@@ -0,0 +1,21 @@
+package com.mocacong.godsaeng.view.detail
+
+import BaseActivity
+import com.mocacong.godsaeng.R
+import com.mocacong.godsaeng.databinding.ActivityDetailBinding
+import com.mocacong.godsaeng.viewmodel.DetailViewModel
+
+class DetailActivity : BaseActivity<ActivityDetailBinding, DetailViewModel>(R.layout.activity_detail) {
+    override val viewModel: DetailViewModel
+        get() = TODO("Not yet implemented")
+
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initListener() {
+        TODO("Not yet implemented")
+    }
+
+
+}
---
+++ android/GodSeang/app/src/main/java/com/mocacong/godsaeng/viewmodel/DetailViewModel.kt
@@ -0,0 +1,6 @@
+package com.mocacong.godsaeng.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class DetailViewModel : ViewModel() {
+}
