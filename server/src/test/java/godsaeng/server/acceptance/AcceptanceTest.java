b1b2c7af2f8efcedb332bd3c91636c76e3191393
임지수 <imjisu@imjisuui-MacBookPro.local>
2023-08-16 20:17:26 +0900

    feat: rest assured 테스트 환경 구축

---
+++ server/src/test/java/godsaeng/server/acceptance/AcceptanceTest.java
@@ -0,0 +1,21 @@
+package godsaeng.server.acceptance;
+
+import godsaeng.server.support.DatabaseCleanerCallback;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(DatabaseCleanerCallback.class)
+public class AcceptanceTest {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUP() {
+        RestAssured.port = port;
+    }
+}
