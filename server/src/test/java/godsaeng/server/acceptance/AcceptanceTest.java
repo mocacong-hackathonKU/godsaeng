8d60b7322e049e4657a1b326c1b0a097b5b48ba6
JungWoo <rlawjddn103@naver.com>
2023-08-17 02:15:56 +0900

    fix: test redis configure 적용

--- server/src/test/java/godsaeng/server/acceptance/AcceptanceTest.java
+++ server/src/test/java/godsaeng/server/acceptance/AcceptanceTest.java
@@ -1,14 +1,17 @@
 package godsaeng.server.acceptance;
 
 import godsaeng.server.support.DatabaseCleanerCallback;
+import godsaeng.server.support.TestRedisConfig;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ExtendWith(DatabaseCleanerCallback.class)
+@Import(TestRedisConfig.class)
 public class AcceptanceTest {
 
     @LocalServerPort
