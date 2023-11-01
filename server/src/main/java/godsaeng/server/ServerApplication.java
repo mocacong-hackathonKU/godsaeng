5c2da81d9b63f78231d6dfc6f35c1afe53c1a608
Jisu Lim <69844138+Ji-soo708@users.noreply.github.com>
2023-11-02 00:11:24 +0900

    Infra/cd (#34)
    * fix: 유효하지 않은 메서드 삭제
    
    * fix: 사용하지 않는 설정 삭제
    
    * feat: ci/cd
    
    * feat: ci/cd
    
    ---------
    
    Co-authored-by: 임지수 <imjisu@imjisuui-MacBookPro.local>

--- server/src/main/java/godsaeng/server/ServerApplication.java
+++ server/src/main/java/godsaeng/server/ServerApplication.java
@@ -2,7 +2,6 @@ package godsaeng.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
