22abb51da97757b3b3cad954e2c7c65ffe770e16
Jisu Lim <69844138+Ji-soo708@users.noreply.github.com>
2023-11-01 23:59:08 +0900

    Infra/cd (#33)
    * fix: 유효하지 않은 메서드 삭제
    
    * fix: 사용하지 않는 설정 삭제
    
    * feat: ci/cd
    
    ---------
    
    Co-authored-by: 임지수 <imjisu@imjisuui-MacBookPro.local>

--- server/src/main/java/godsaeng/server/ServerApplication.java
+++ server/src/main/java/godsaeng/server/ServerApplication.java
@@ -9,11 +9,12 @@ import java.util.TimeZone;
 
 @SpringBootApplication
 public class ServerApplication {
+
     @PostConstruct
     public void setSeoulTimeZone() {
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
-
+  
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
     }
